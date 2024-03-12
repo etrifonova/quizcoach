5c37e7f2fdcfee44481a1d1c2959b50cb57ed782
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-03-12 10:22:03 +0300

    fix sth

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -231,12 +231,12 @@ bot.telegram.getMe().then((botInfo) => {
 // }
 
 const response = fetch('https://api.github.com/users/github');
-// const data = response.json();
+const data = await response.json();
 
 bot.start((ctx) => {
   try {
     // questionsPotter = questions.slice(0);
-    console.log(response)
+    console.log(data)
     // console.log("Осталось вопросов:" + questionsPotter.length);
     return ctx.reply("Hello there");
   } catch (e) {
