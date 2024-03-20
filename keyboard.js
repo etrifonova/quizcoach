b4a5ae793b07c0a59c5d7f185612787db5c735fa
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-03-20 14:26:13 +0300

    kb fix

--- keyboard.js
+++ keyboard.js
@@ -2,7 +2,7 @@ const { Markup } = require("telegraf");
 
 function getMenu() {
   return Markup.keyboard([
-    ["/question", "/answer", "wft"], // Row1 with 2 buttons
+    ["/question", "/answer"],
   ])
     .oneTime()
     .resize()
