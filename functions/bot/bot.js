757ca07f7333d3e623fbf0fc6dc0d00a95f6657f
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2024-03-21 17:21:26 +0300

    m

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -35,9 +35,7 @@ randomElement = generateQuestion();
 bot.command("question", (ctx) => {
   ctx.reply(randomElement.question, 
 		Markup.inlineKeyboard([
-			Markup.button.callback("Coke", "Coke"),
-			Markup.button.callback("Dr Pepper", "Dr Pepper", Math.random() > 0.5),
-			Markup.button.callback("Pepsi", "Pepsi"),
+			Markup.button.callback("/answer", "/answer")
 		]),);
 });
 
