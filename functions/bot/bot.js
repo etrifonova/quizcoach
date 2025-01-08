9328b2da30088f3ae10877d8152a6af90291630f
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2025-01-08 12:50:51 +0300

    upd

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -4,7 +4,6 @@ const questions = require("../../questions.js");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const getKeyboard = require("../../keyboard.js");
 
-
 bot.telegram.getMe().then((botInfo) => {
   bot.options.username = botInfo.username;
 });
@@ -20,7 +19,7 @@ let randomElement;
 const categories = [...new Set(questions.map((q) => q.category))];
 
 bot.start((ctx) => {
-  try {    
+  try {
     return ctx.reply(
       "Выберите категорию:",
       Markup.inlineKeyboard(
@@ -39,65 +38,63 @@ bot.action(/CATEGORY_(.+)/, (ctx) => {
   const selectedCategory = ctx.match[1];
   currentQuestions = questions.filter(
     (question) => question.category === selectedCategory
-  );
+  )[0];
+  console.log(currentQuestions);
 
   if (currentQuestions.length > 0) {
-  randomElement = currentQuestions[0].questions.map((element) => element)[
-    Math.floor(Math.random() * currentQuestions.length)
-  ];
+    randomElement = currentQuestions[0].questions.map((element) => element)[
+      Math.floor(Math.random() * currentQuestions.length)
+    ];
     console.log(randomElement);
     return ctx.reply(
       `Категория: ${selectedCategory}. Поехали!`,
-      Markup.inlineKeyboard([
-        Markup.button.callback("Вопрос", "Вопрос"),
-      ])
+      Markup.inlineKeyboard([Markup.button.callback("Вопрос", "Вопрос")])
     );
   } else {
     return ctx.reply("В этой категории пока нет вопросов.");
   }
 });
 
-
 // randomElement = generateQuestion();
 
 // bot.command("question", (ctx) => {
-//   ctx.reply(randomElement.question, 
+//   ctx.reply(randomElement.question,
 // 		Markup.inlineKeyboard([
 // 			Markup.button.callback("Ответ", "Ответ"),
 //       Markup.button.callback("Подсказка", "Подсказка")
 // 		]),);
 // });
 
-// bot.command("answer", (ctx) => 
+// bot.command("answer", (ctx) =>
 // ctx.reply(randomElement.answer)
 // )
 
-bot.action("Вопрос", ctx => 
-{
+bot.action("Вопрос", (ctx) => {
   try {
-ctx.reply(randomElement.question, 
-  Markup.inlineKeyboard([
-    Markup.button.callback("Ответ", "Ответ"),
-    Markup.button.callback("Подсказка", "Подсказка")
-  ]))
+    ctx.reply(
+      randomElement.question,
+      Markup.inlineKeyboard([
+        Markup.button.callback("Ответ", "Ответ"),
+        Markup.button.callback("Подсказка", "Подсказка"),
+      ])
+    );
   } catch (e) {
     console.error("error in start action:", e);
     return ctx.reply("NOT WORKING");
-  }}
+  }
+});
 
-)
+bot.action("Ответ", (ctx) => ctx.reply(randomElement.answer));
 
-bot.action("Ответ", ctx => ctx.reply(randomElement.answer))
+bot.action("Комментарий", (ctx) => ctx.reply(randomElement.comment));
 
-bot.action("Комментарий", ctx => ctx.reply(randomElement.comment))
-
-bot.action("Подсказка", ctx => ctx.reply(randomElement.hint))
+bot.action("Подсказка", (ctx) => ctx.reply(randomElement.hint));
 
 bot.on("message", (ctx) => {
   let userAnswer = ctx.message.text.toLowerCase();
   let correctAnswer = randomElement.answer.toLowerCase();
   let comment = randomElement.comment;
-  
+
   if (currentQuestions.length == 1 && userAnswer === correctAnswer) {
     currentQuestions.splice(currentQuestions.indexOf(randomElement), 1);
     randomElement = currentQuestions.questions;
@@ -107,10 +104,10 @@ bot.on("message", (ctx) => {
   } else if (currentQuestions.length > 1 && userAnswer === correctAnswer) {
     currentQuestions.splice(currentQuestions.indexOf(randomElement), 1);
     randomElement = currentQuestions.questions;
-    ctx.reply("Верно!" + " \n" + comment, 
-		Markup.inlineKeyboard([
-			Markup.button.callback("Вопрос", "Вопрос")
-		]));
+    ctx.reply(
+      "Верно!" + " \n" + comment,
+      Markup.inlineKeyboard([Markup.button.callback("Вопрос", "Вопрос")])
+    );
   } else {
     ctx.reply("Неверно!");
   }
